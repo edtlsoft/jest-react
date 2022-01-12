ec29ec9634205ea3d6c6cb8e9d5d374d0abb47b9
Edward Lopez <edward.trigos@nivelics.co>
2022-01-12 14:13:27 -0500

    [add] Mocks

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
 
-describe('Testing footer', () => {
+describe('<Footer />', () => {
+  const footer = mount(<Footer />);
+
   test('should be rendered', () => {
-    const footer = mount(<Footer />);
     expect(footer.length).toEqual(1);
+  });
+
+  test('should be render the title', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
   });
 });
